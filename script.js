84efc3879f907b064495e0fdea2ccde72705fda5
James Zhou <jam9es@gmail.com>
2023-07-12 22:23:50 -0700

    Expand on getComputerChoice to make it functional

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Write a function to get computer's choice
 function getComputerChoice () {
-
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
 }
+
+console.log(getComputerChoice());
