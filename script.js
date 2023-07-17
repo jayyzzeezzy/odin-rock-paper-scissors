c16e300ecb524677f5031aa869a8e4d19cdd63b5
James Zhou <jam9es@gmail.com>
2023-07-17 10:34:59 -0700

    Write game function to play 5 rounds

--- script.js
+++ script.js
@@ -43,6 +43,128 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// function that plays 5 rounds, keep score, and annouce winner
+function game () {
+    let playerWinCounts = 0;
+    let computerWinCounts = 0;
+    let tieCounts = 0;
+    // round 1
+    let computerSelection = getComputerChoice ();
+    let playerSelection = prompt ('Please make a selection: rock, paper, or scissors?');
+    let result = findWinner (playerSelection, computerSelection);
+    if (result == 'tie') {
+        tieCounts += 1;
+        console.log ('It\'s a tie.')
+    }
+    else if (result == 'player') {
+        playerWinCounts += 1;
+        console.log ('Player win!')
+    }
+    else if (result == 'computer') {
+        computerWinCounts += 1;
+        console.log ('Computer win!')
+    }
+    else {
+        console.log ('Invalid entry')
+    }
+    console.log (computerSelection)
+    console.log (playerSelection)
+    // round 2
+    computerSelection = getComputerChoice ();
+    playerSelection = prompt ('Please make a selection: rock, paper, or scissors?');
+    result = findWinner (playerSelection, computerSelection);
+    if (result == 'tie') {
+        tieCounts += 1;
+        console.log ('It\'s a tie.')
+    }
+    else if (result == 'player') {
+        playerWinCounts += 1;
+        console.log ('Player win!')
+    }
+    else if (result == 'computer') {
+        computerWinCounts += 1;
+        console.log ('Computer win!')
+    }
+    else {
+        console.log ('Invalid entry')
+    }
+    console.log (computerSelection)
+    console.log (playerSelection)
+    // round 3
+    computerSelection = getComputerChoice ();
+    playerSelection = prompt ('Please make a selection: rock, paper, or scissors?');
+    result = findWinner (playerSelection, computerSelection);
+    if (result == 'tie') {
+        tieCounts += 1;
+        console.log ('It\'s a tie.')
+    }
+    else if (result == 'player') {
+        playerWinCounts += 1;
+        console.log ('Player win!')
+    }
+    else if (result == 'computer') {
+        computerWinCounts += 1;
+        console.log ('Computer win!')
+    }
+    else {
+        console.log ('Invalid entry')
+    }
+    console.log (computerSelection)
+    console.log (playerSelection)
+    // round 4
+    computerSelection = getComputerChoice ();
+    playerSelection = prompt ('Please make a selection: rock, paper, or scissors?');
+    result = findWinner (playerSelection, computerSelection);
+    if (result == 'tie') {
+        tieCounts += 1;
+        console.log ('It\'s a tie.')
+    }
+    else if (result == 'player') {
+        playerWinCounts += 1;
+        console.log ('Player win!')
+    }
+    else if (result == 'computer') {
+        computerWinCounts += 1;
+        console.log ('Computer win!')
+    }
+    else {
+        console.log ('Invalid entry')
+    }
+    console.log (computerSelection)
+    console.log (playerSelection)
+    // round 5
+    computerSelection = getComputerChoice ();
+    playerSelection = prompt ('Please make a selection: rock, paper, or scissors?');
+    result = findWinner (playerSelection, computerSelection);
+    if (result == 'tie') {
+        tieCounts += 1;
+        console.log ('It\'s a tie.')
+    }
+    else if (result == 'player') {
+        playerWinCounts += 1;
+        console.log ('Player win!')
+    }
+    else if (result == 'computer') {
+        computerWinCounts += 1;
+        console.log ('Computer win!')
+    }
+    else {
+        console.log ('Invalid entry')
+    }
+    console.log (computerSelection)
+    console.log (playerSelection)
+
+    // annouce final winner
+    if (playerWinCounts == computerWinCounts) {
+        console.log ('Tie')
+    }
+    else if (playerWinCounts > computerWinCounts) {
+        console.log ('Final winner is player!')
+    }
+    else {
+        console.log ('Final winner is computer!')
+    }
+}
+
+// call the game function to run 5 rounds of rock paper scissors
+game ();
